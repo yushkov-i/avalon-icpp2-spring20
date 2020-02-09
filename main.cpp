3c1af47dd176b1f806710ce3f22170bf460232dc
dimadgio <60820730+dimadgio@users.noreply.github.com>
2020-02-09 21:32:33 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,8 +5,31 @@ using namespace std;
 int main()
 {
     setlocale(0,"rus");
-   int number=0,sum=0,chanching=0;
-   cout << "Введите ваш ISBN-10 " << endl;
+   long long number=0,chanching=0;
+   int sum=0;
+    cout << "enter your ISBN-13 " << endl;
+   cin>>number;
+   chanching=number/10;
+    for(int i=12;i>0;i--)
+    {
+      int a=chanching%10;
+      chanching=chanching/10;
+
+      if (i%2==0)
+      {
+           sum=sum+(a*3);
+      }
+      else
+      {
+          sum+=a;
+      }
+    }
+if((10-(sum%10)==number%10))
+    cout<<"valid ISBN-13\n";
+    else
+    cout<<"invalid ISBN-13\n";
+    //дополнительно
+  /* cout << "enter your ISBN-10 " << endl;
    cin>>number;
    chanching=number/10;
     for(int i=10,j=2;i>1;i--,j++)
@@ -16,8 +39,9 @@ int main()
       sum+=a*j;
     }
 if((11-(sum%11)==number%10))
-    cout<<"действительный ISBN-10\n";
+    cout<<"valid ISBN-10\n";
     else
-    cout<<"недействительный ISBN-10\n";
+    cout<<"invalid ISBN-10\n";*/
+
     return 0;
 }
