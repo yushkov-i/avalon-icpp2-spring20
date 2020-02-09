2ff1891b4c84ef396dd3a84fa645661146d77b21
dimadgio <60820730+dimadgio@users.noreply.github.com>
2020-02-09 15:47:39 +0300

    дополнтельно

--- main.cpp
+++ main.cpp
@@ -1,24 +1,23 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
+
 int main()
 {
-	cout << "Hello world!" << endl;
-	cout << "Hello to you too!" << endl;
-
-
-
-	return 0;
+    setlocale(0,"rus");
+   int number=0,sum=0,chanching=0;
+   cout << "¬ведите ваш ISBN-10 " << endl;
+   cin>>number;
+   chanching=number/10;
+    for(int i=10,j=2;i>1;i--,j++)
+    {
+      int a=chanching%10;
+      chanching=chanching/10;
+      sum+=a*j;
+    }
+if((11-(sum%11)==number%10))
+    cout<<"действительный ISBN-10\n";
+    else
+    cout<<"недействительный ISBN-10\n";
+    return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
