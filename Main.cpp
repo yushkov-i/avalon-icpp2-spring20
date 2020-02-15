a4108e7f5efb65a58d31b1eac1dfb5616fcbd31a
vladislav-bordiug <60838512+vladislav-bordiug@users.noreply.github.com>
2020-02-15 21:34:56 +0300

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,36 @@
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
+
 int main()
 {
-	cout << "Hello world!" << endl;
-	cout << "and " << endl;
+	int N;
+	cout << "Enter N" << endl;
+	cin >> N;
+	cout << "ISBN-13:" << endl;
+	for (int i = 0; i < N; i++) {
+		int long long isbn, isbn1, isbn2;
+		int k = 0, end, sum = 0, number, remains, num;
+		cin >> isbn;
+		isbn1 = isbn;
+		end = isbn % 10;
+		isbn = isbn / 10;
+		isbn2 = isbn * 10;
+		do {
+			number = isbn % 10;
+			isbn /= 10;
+			if (k % 2 == 0)
+				sum += number * 3;
+			else
+				sum += number * 1;
+			k++;
+		} while (isbn >= 1);
+		remains = sum % 10;
+		num = 10 - remains;
+		isbn2 += num;
+		if (isbn2 == isbn1)
+			cout << "valid" << endl;
+		else
+			cout << "invalid" << endl;
+	}
+	return 0;
 }
