bd7ad014669217284f43ac6bbd7781a133d0cc8f
nikvin112 <61106893+nikvin112@users.noreply.github.com>
2020-02-16 13:26:48 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include "Extention.hpp"
 using namespace std;
+using namespace ext;
+
 
 enum Grade
 {
@@ -9,11 +12,23 @@ enum Grade
 	B = 4,  // хорошо
 	A = 5,  // отлично      
 };
+struct Student
+{
+	int Id;
+	Grade Mark;
+	char Name[7];
+};
 int main()
 {
-	Grade mark = Grade::B;
-	cout << mark << endl;
-	int temp;
-	cin >> temp;
-	mark = static_cast<Grade> (temp);
+	Student student;
+	student.Id = GetRandomValue(1, 1000);
+	student.Mark = static_cast<Grade> (GetRandomValue(1, 5));
+	int length = GetRandomValue(3, 6);
+	for (int i = 0; i < length; i++)
+	{
+		student.Name[i] = GetRandomValue('a', 'z');
+
+	}
+	student.Name[length] = '\0';
+
 }
