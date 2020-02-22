f3eb390853820888036a5ea09b8117973b077f69
nickspb-ui <55831368+nickspb-ui@users.noreply.github.com>
2020-02-22 16:10:17 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -7,11 +7,11 @@ using namespace ext;
 
 enum Grade
 {
-	F = 1,   // ������
-	D = 2,  // �������������������
-	C = 3,  // �����������������
-	B = 4,  // ������
-	A = 5,  // �������      
+	F = 1,   // неявка
+	D = 2,  // неудовлетворительно
+	C = 3,  // удовлетворительно
+	B = 4,  // хорошо
+	A = 5,  // отлично      
 };
 
 struct Student
@@ -23,14 +23,16 @@ struct Student
 
 int main() {
 	char length;
-	int type_sort, x;
+	int type_sort, x, summ{};
 	bool k = 0;
-	bool higher_name = 0;;
+	bool higher_name = 0;
+	int num_of_grades[5]{};
 	char tmp[7]{};
 	setlocale(LC_ALL, "Rus");
 	Student list[100];
 	for (int i = 0; i < 100; i++) {
-			list[i].Id = GetRandomValue(1, 1000);		
+			list[i].Id = GetRandomValue(1, 1000);	
+			k = 0;
 			while (k == 0) {
 				k = 1;
 				for (int j = 0; j < i; j++) {
@@ -48,9 +50,28 @@ int main() {
 			}
 			cout << list[i].Name << ", ";
 			list[i].Mark = static_cast<Grade>(GetRandomValue(1, 5));
-			cout << list[i].Mark << endl;
+			if (list[i].Mark == 1) {
+				cout << 'F' << endl;
+				num_of_grades[0]++;
+			}
+			if (list[i].Mark == 2) {
+				cout << 'D' << endl;
+				num_of_grades[1]++;
+			}
+			if (list[i].Mark == 3) {
+				cout << 'C' << endl;
+				num_of_grades[2]++;
+			}
+			if (list[i].Mark == 4) {
+				cout << 'B' << endl;
+				num_of_grades[3]++;
+			}
+			if (list[i].Mark == 5) {
+				cout << 'A' << endl;
+				num_of_grades[4]++;
+			}
 	}
-	cout << "������� 1 ��� ���������� �� ID, 2 - �� Mark, 3 - �� Name: ";
+	cout << "Введите '1' для сортировки по ID, '2' - по оценкам, '3' - по именам: ";
 	cin >> type_sort;
 	k = 0;
 	switch (type_sort) {
@@ -151,7 +172,105 @@ int main() {
 	}
 	for (int i = 0; i < 100; i++) {
 		cout << list[i].Id << " - ";
+		summ += list[i].Mark;
 		cout << list[i].Name << ", ";
-		cout << list[i].Mark << endl;
+		if (list[i].Mark == 1) {
+			cout << 'F' << endl;
+		}
+		if (list[i].Mark == 2) {
+			cout << 'D' << endl;
+		}
+		if (list[i].Mark == 3) {
+			cout << 'C' << endl;
+		}
+		if (list[i].Mark == 4) {
+			cout << 'B' << endl;
+		}
+		if (list[i].Mark == 5) {
+			cout << 'A' << endl;
+		}
+	}
+	cout << "\nСредний балл: " << summ / 100.00 << endl;
+	cout << "Количество оценок 'F': " << num_of_grades[0] << endl;
+	cout << "Количество оценок 'D': " << num_of_grades[1] << endl;
+	cout << "Количество оценок 'C': " << num_of_grades[2] << endl;
+	cout << "Количество оценок 'B': " << num_of_grades[3] << endl;
+	cout << "Количество оценок 'A': " << num_of_grades[4] << endl;
+	summ = 0;
+	char p = 0;
+	while (1) {
+		if (num_of_grades[p] + summ >= 25) {
+			cout << "Чтобы стать лучше 25% студентов нужно получить оценку ";
+			if (p == 1) {
+				cout << 'F' << " или выше " << endl;
+			}
+			if (p == 2) {
+				cout << 'D' << " или выше " << endl;
+			}
+			if (p == 3) {
+				cout << 'C' << " или выше " << endl;
+			}
+			if (p == 4) {
+				cout << 'B' << " или выше " << endl;
+			}
+			if (p == 0) {
+				cout << 'A' << " или выше " << endl;
+			}
+			break;
+		}
+		else {
+			summ += num_of_grades[p];
+			p++;
+		}
+	}
+	while (1) {
+		if (num_of_grades[p] + summ >= 50) {
+			cout << "Чтобы стать лучше 50% студентов нужно получить оценку ";
+			if (p == 1) {
+				cout << 'F' << " или выше " << endl;
+			}
+			if (p == 2) {
+				cout << 'D' << " или выше " << endl;
+			}
+			if (p == 3) {
+				cout << 'C' << " или выше " << endl;
+			}
+			if (p == 4) {
+				cout << 'B' << " или выше " << endl;
+			}
+			if (p == 0) {
+				cout << 'A' << " или выше " << endl;
+			}
+			break;
+		}
+		else {
+			summ += num_of_grades[p];
+			p++;
+		}
+	}
+	while (1) {
+		if (num_of_grades[p] + summ >= 75) {
+			cout << "Чтобы стать лучше 75% студентов нужно получить оценку ";
+			if (p == 1) {
+				cout << 'F' << " или выше " << endl;
+			}
+			if (p == 2) {
+				cout << 'D' << " или выше " << endl;
+			}
+			if (p == 3) {
+				cout << 'C' << " или выше " << endl;
+			}
+			if (p == 4) {
+				cout << 'B' << " или выше " << endl;
+			}
+			if (p == 0) {
+				cout << 'A' << " или выше " << endl;
+			}
+			break;
+		}
+		else {
+			summ += num_of_grades[p];
+			p++;
+		}
 	}
 }
