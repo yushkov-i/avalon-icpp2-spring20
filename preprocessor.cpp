f0d31079197714f66fdbf28bedc38a59e9dbf43d
warmike01 <51020399+warmike01@users.noreply.github.com>
2020-02-22 19:42:49 +0300

    Update preprocessor.cpp

--- preprocessor.cpp
+++ preprocessor.cpp
@@ -3,10 +3,15 @@
 #define true false
 #endif
 using namespace std;
+int minus(int a; int b);
 int main()
 {
 #ifdef __WIN32
 	bool a = true;
 	bool b = false;
 #endif
+}
+int minus(int a; int b)
+{
+	return a-b;
 }
