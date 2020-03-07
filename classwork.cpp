eda55e146484fcf40c697eae468dc35a8ffaea25
yushkov-i <47275068+yushkov-i@users.noreply.github.com>
2020-03-07 21:24:13 +0300

    Add files via upload

---
+++ classwork.cpp
@@ -0,0 +1,118 @@
+#include <cstdio>
+#include <limits>
+#include <iostream>
+
+using namespace std;
+
+void swap(int& a, int& b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+int maxValue(int data[], int size)
+{
+	int tempMax = data[0];
+	int index = 0;
+	for (int i = 1; i < size; ++i)
+	{
+		if (data[i] > tempMax)
+		{
+			tempMax = data[i];
+			index = i;
+		}
+	}
+	return index;
+}
+
+int* maxValuePtr(int data[], int size)
+{
+	int* ptr = &data[0];
+	for (int i = 1; i < size; ++i)
+	{
+		if (*ptr < data[i])
+		{
+			ptr = &data[i];
+		}
+	}
+
+	return ptr;
+}
+
+int& maxValueRef(int data[], int size)
+{
+	int tempMax = data[0];
+	int index = 0;
+	for (int i = 1; i < size; ++i)
+	{
+		if (data[i] > tempMax)
+		{
+			tempMax = data[i];
+			index = i;
+		}
+	}
+	return data[index];
+}
+
+int& max(int& a, int& b)
+{
+	return a > b ? a : b;
+}
+
+struct Point
+{
+	int x;
+	int y;
+};
+
+void printPoint(Point point)
+{
+	cout << "x = " << point.x << endl;
+	cout << "y = " << point.y << endl;
+}
+
+Point operator ++(Point& p)
+{
+	++(p.x);
+	++(p.y);
+	return p;
+}
+
+Point operator ++(Point& p, int)
+{
+	Point temp = p;
+	++p.x;
+	++p.y;
+	return temp;	
+}
+
+int main()
+{
+	Point p{ 10,15 };
+	
+	Point pp = p++;
+	Point ppp = p++;	
+
+	int a = 10;
+	int b = a++;
+
+	printPoint(p);
+
+	int data[5] = { 5,4,7,3,6 };
+
+	int maxIndex = maxValue(data, 5);
+	data[maxIndex] *= 2;
+
+	int* maxPtr = maxValuePtr(data, 5);
+	*maxPtr *= 2;
+
+	int& maxRef = maxValueRef(data, 5);
+	maxRef = maxRef * 2;
+
+	int a = 10;
+	int b = 15;
+
+	int& maxValue = max(a, b);
+	maxValue *= 2;	
+}
---
+++ date-print.cpp
@@ -0,0 +1,20 @@
+#include "Date.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace ext;
+
+int main()
+{
+	//Date date{ 12, static_cast<Month>(2), 1998 }; // плохо
+	Date date{ 1998, Month::February, 12 };
+
+	print(date, DateFormat::MonthAsInt);	// 12.2.1998
+	cout << endl;
+	print(date, DateFormat::MonthAsString);	// 12 february 1998
+
+	cout << endl;
+	print(date.month, DateFormat::MonthAsInt); // 2
+	cout << endl;
+	print(date.month, DateFormat::MonthAsString); // february
+}
