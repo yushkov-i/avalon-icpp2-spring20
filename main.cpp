f348e5efaa8d2aeb05ddf5c8b8557428ae7d568c
nickspb-ui <55831368+nickspb-ui@users.noreply.github.com>
2020-05-05 13:52:19 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,31 +1,36 @@
 #include <iostream>
+#include "Header.hpp"
 
 using namespace std;
-
-struct Node {
-    char symb;
-    Node *Next;
-};
+using namespace ext;
 
 int main() {
-    Node *first = new Node {'h'};
-    Node *second = new Node {'e'};
-    first->Next = second;
-    Node *third = new Node {'l'};
-    second->Next = third;
-    Node *fourth = new Node {'l'};
-    third->Next = fourth;
-    Node *fifth = new Node {'o'};
-    fourth->Next = fifth;
-    int num, count{};
-    Node* curr = first;
-    cin >> num;
-    while(count != num) {
-        if (count == num - 1) {
-            cout << curr->symb;
-        } else {
-            curr = curr->Next;
-        }
-        count++;
-    }
+	setlocale(LC_ALL, "Rus");
+	char x;
+	Node* first = new Node{ 'h' };
+	Node* second = new Node{ 'e' };
+	first->Next = second;
+	Node* third = new Node{ 'l' };
+	second->Next = third;
+	Node* fourth = new Node{ 'l' };
+	third->Next = fourth;
+	Node* fifth = new Node{ 'o' };
+	fourth->Next = fifth;
+	fifth->Next = nullptr;
+	int num, count{};
+	cout << "Введите номер символа, который хотите получить из строки: ";
+	cin >> num;
+	cout << "Символ под номером " << num << ": " << characterAt(first, num) << endl;
+	cout << "Введите символ, который хотите добавить в конец строки: ";
+	cin >> x;
+	cout << "Готовая строка: \"";
+	Node* sixth = append(first, x);
+	fifth->Next = sixth;
+	sixth->Next = nullptr;
+	Node* tmp = first;
+	while (tmp != nullptr) {
+		cout << tmp->symb;
+		tmp = tmp->Next;
+	}
+	cout << "\"" << endl;
 }
