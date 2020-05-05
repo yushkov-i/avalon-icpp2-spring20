ad9e325e28555f9e6cac672119b0e6d9353ba4b5
nickspb-ui <55831368+nickspb-ui@users.noreply.github.com>
2020-05-05 15:32:32 +0300

    Update Header.hpp

--- Header.hpp
+++ Header.hpp
@@ -27,4 +27,37 @@ namespace ext
 		Node* tail = new Node{symbb};
 		return tail;
 	}
+
+	int countSize(Node* tmp) {
+		int size{};
+		Node* curr = tmp;
+		while (curr != nullptr) {
+				size++;
+				curr = curr->Next;
+		}
+		return size;
+	}
+
+	char* removeChars(Node* head, int a, int cost) {
+		int size_was = countSize(head);
+		int size = size_was - cost;	
+		char x;
+		char* tmp = new char[size + 1];
+		Node* curr_pos = head;
+		int count = 0;
+		for (int i = 0; i < size_was; i++) {
+			if ((i >= a - 1) && (i < a - 1 + cost)) {
+				curr_pos = curr_pos->Next;
+			}
+			else {
+				x = curr_pos->symb;
+				tmp[count] = x;
+				curr_pos = curr_pos->Next;
+				count++;
+			}
+		}
+		tmp[size] = '\0';
+		return tmp;
+	}
+
 }
