0bae2c0cfb69a101a8d3c085d4b318080b1dc6b9
nyandavidd <60820852+nyandavidd@users.noreply.github.com>
2020-05-26 01:39:02 +0300

    Add files via upload

---
+++ hw9.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+struct Triangle{
+	int a;
+	int b;
+	int c;
+	double thickness;
+};
+
+const int TRIANGLE_SIZE = sizeof(Triangle);
+
+int main()
+{
+	int N;
+	cin >> N;
+	ifstream input;
+	input.open("triangle.bin", ios::binary);
+	if (!input.is_open()){
+		cerr << "triangle.bin File did not open!";
+		return 1;
+	}
+	Triangle triangle;
+	input.seekg(N * TRIANGLE_SIZE);
+	input.read(reinterpret_cast<char*>(&triangle), sizeof(Triangle));
+	input.close();
+	cout << "a = " << triangle.a <<
+	endl << "b = " << triangle.b << 
+	endl<< "c = " << triangle.c << 
+	endl << "thickness = " << triangle.thickness << endl;
+}
+
