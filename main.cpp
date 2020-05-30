0c8979838a6a577ce6d3aaab12b17010f7e27dfb
Quarysite <60838492+Quarysite@users.noreply.github.com>
2020-05-30 12:32:59 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,31 @@
 #include<iostream>
-
+#include<fstream>
 
 using namespace std;
+
+struct Triangle
+{
+	int a;
+	int b;
+	int c;
+	double thickness;
+};
+
 int main()
 {
-
-	cout << "Hello world!";
-
-
+	cout << "Input the Triangle number: ";
+	int trg =0;
+	cin >> trg;
+	cout << endl;
+	ifstream TrigIn;
+	TrigIn.open("triangle.bin", ios::binary);
+	Triangle triangl;
+	TrigIn.seekg(trg * sizeof(Triangle));
+	TrigIn.read(reinterpret_cast<char*>(&triangl), sizeof(Triangle));
+	TrigIn.close();
+	cout << "Your Triangle date is : " << endl;
+	cout << "Fs : " << triangl.a << endl;
+	cout << "Ss : " << triangl.b << endl;
+	cout << "Ts : " << triangl.c << endl;
+	cout << "Triangle thickness : " << triangl.thickness << endl;
 }
-
-
-
-
-
-
-
